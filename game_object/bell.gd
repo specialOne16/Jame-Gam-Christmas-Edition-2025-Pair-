c2b7b320363f9df96aaea1308988cdf5ff2501b9
Jun <jundi2303@gmail.com>
2025-12-21 19:27:10 +0700

    use timer to stop bell

--- game_object/bell.gd
+++ game_object/bell.gd
@@ -8,8 +8,8 @@ signal ring(is_ringing: bool)
 func _on_area_entered(_area: Area2D) -> void:
 	ring.emit(true)
 	animated_sprite_2d.play("ring")
+	$BellStopTimer.start()
 
-
-func _on_area_exited(_area: Area2D) -> void:
+func _on_bell_stop_timer_timeout() -> void:
 	ring.emit(false)
 	animated_sprite_2d.play("default")
