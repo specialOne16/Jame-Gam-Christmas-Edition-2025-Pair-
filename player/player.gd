5f356581e5ce0457fa8c15d6e4395d7d2dab8968
Jun <jundi2303@gmail.com>
2025-12-23 20:06:25 +0700

    light and hidden object

--- player/player.gd
+++ player/player.gd
@@ -7,27 +7,27 @@ const MIN_BRIGHTNESS = 0.2
 const MAX_BRIGHTNESS = 2.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var light: PointLight2D = %Light
 
 var target_light
 var level_finished = false
 
 func _ready() -> void:
 	target_light = MIN_BRIGHTNESS
-	point_light_2d.texture_scale = target_light
+	light.texture_scale = target_light
 
 func brighten_light(on):
 	if on: target_light = MAX_BRIGHTNESS
 
 func _process(delta: float) -> void:
 	if level_finished:
-		point_light_2d.texture_scale = move_toward(point_light_2d.texture_scale, 10, delta * BRIGHT_UP_SPEED)
+		light.texture_scale = move_toward(light.texture_scale, 10, delta * BRIGHT_UP_SPEED)
 		return
 	
-	var speed = BRIGHT_DOWN_SPEED if point_light_2d.texture_scale > target_light else BRIGHT_UP_SPEED
-	point_light_2d.texture_scale = move_toward(point_light_2d.texture_scale, target_light, delta * speed)
+	var speed = BRIGHT_DOWN_SPEED if light.texture_scale > target_light else BRIGHT_UP_SPEED
+	light.texture_scale = move_toward(light.texture_scale, target_light, delta * speed)
 	
-	if point_light_2d.texture_scale == target_light:
+	if light.texture_scale == target_light:
 		target_light = MIN_BRIGHTNESS
 
 func _physics_process(_delta: float) -> void:
