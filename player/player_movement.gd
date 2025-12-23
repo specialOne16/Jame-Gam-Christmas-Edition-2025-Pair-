33fb4d46eea85842c785e551792b2650dee41cc6
Jun <jundi2303@gmail.com>
2025-12-23 19:19:33 +0700

    basic map

--- player/player_movement.gd
+++ player/player_movement.gd
@@ -3,7 +3,7 @@ class_name PlayerMovement
 
 @export var player: Player
 
-const movement_speed = 100
+const movement_speed = 300
 
 var last_direction = "front"
 
@@ -14,11 +14,10 @@ func _physics_process(_delta: float) -> void:
 		return
 	
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	player.velocity = input_direction * movement_speed
 	
 	if input_direction == Vector2.ZERO: player.animated_sprite_2d.play("idle_%s" % get_direction_name())
 	else: player.animated_sprite_2d.play("move_%s" % get_direction_name())
-	
-	player.velocity = input_direction * movement_speed
 
 func get_direction_name() :
 	var direction
